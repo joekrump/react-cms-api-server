32361cc13f67ce7fd71f771b5037a47a79355682
Joseph Krump <krump.joseph@gmail.com>
2016-07-22 17:15:15 -0700

    added show, update and destroy methods to books controller

--- app/Api/V1/Controllers/BookController.php
+++ app/Api/V1/Controllers/BookController.php
@@ -38,4 +38,47 @@ class BookController extends Controller
       else
           return $this->response->error('could_not_create_book', 500);
   }
+
+  public function show($id)
+  {
+      $currentUser = JWTAuth::parseToken()->authenticate();
+
+      $book = $currentUser->books()->find($id);
+
+      if(!$book)
+          throw new NotFoundHttpException; 
+
+      return $book;
+  }
+
+  public function update(Request $request, $id)
+  {
+      $currentUser = JWTAuth::parseToken()->authenticate();
+
+      $book = $currentUser->books()->find($id);
+      if(!$book)
+          throw new NotFoundHttpException;
+
+      $book->fill($request->all());
+
+      if($book->save())
+          return $this->response->noContent();
+      else
+          return $this->response->error('could_not_update_book', 500);
+  }
+
+  public function destroy($id)
+  {
+      $currentUser = JWTAuth::parseToken()->authenticate();
+
+      $book = $currentUser->books()->find($id);
+
+      if(!$book)
+          throw new NotFoundHttpException;
+
+      if($book->delete())
+          return $this->response->noContent();
+      else
+          return $this->response->error('could_not_delete_book', 500);
+  }
 }
