691ed6d1e5c493fcaea679be161b5832c1a1a3e3
Joseph Krump <krump.joseph@gmail.com>
2016-07-23 14:58:23 -0700

    routes moved into groups

--- app/Http/api_routes.php
+++ app/Http/api_routes.php
@@ -1,28 +1,22 @@
 <?php
 	
-	$api = app('Dingo\Api\Routing\Router');
+$api = app('Dingo\Api\Routing\Router');
 
-	$api->group(['middleware' => 'api.auth', 'version' => 'v1'], function ($api) {
-		// resource creates all RESTful CRUD routes
-		$api->resource('books', 'App\Api\V1\Controllers\BookController'); 
-	});
+$api->group(['middleware' => ['api.auth', 'cors'], 'version' => 'v1'], function ($api) {
+	// resource creates all RESTful CRUD routes
+	$api->resource('books', 'App\Api\V1\Controllers\BookController'); 
 
-	$api->version('v1', function ($api) {
-
-	$api->post('auth/login', 'App\Api\V1\Controllers\AuthController@login')->middleware('cors');
-	$api->post('auth/signup', 'App\Api\V1\Controllers\AuthController@signup')->middleware('cors');
-	$api->post('auth/recovery', 'App\Api\V1\Controllers\AuthController@recovery')->middleware('cors');
-	$api->post('auth/reset', 'App\Api\V1\Controllers\AuthController@reset')->middleware('cors');
 
 	// example of protected route
-	$api->get('protected', ['middleware' => ['auth-cors'], function () {		
+	$api->get('protected', function () {		
 		return \App\User::all();
-  }]);
+  });
+});
 
-	
-	// example of free route
-	$api->get('free', ['middleware' => ['cors'], function() {
-		return \App\User::all();
-	}]);
+$api->group(['middleware' => 'cors', 'version' => 'v1'], function ($api) {
 
+	$api->post('auth/login', 'App\Api\V1\Controllers\AuthController@login');
+	$api->post('auth/signup', 'App\Api\V1\Controllers\AuthController@signup');
+	$api->post('auth/recovery', 'App\Api\V1\Controllers\AuthController@recovery');
+	$api->post('auth/reset', 'App\Api\V1\Controllers\AuthController@reset');
 });
