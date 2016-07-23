8d0023b4155d0755d221c1dbeca8accb34c326ea
Joseph Krump <krump.joseph@gmail.com>
2016-07-23 15:13:32 -0700

    entrust migration and seeder added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+      Model::unguard();
+
+      DB::table(Config::get('auth.table'))->delete();
+
+      $faker = Faker\Factory::create();
+      $limit = 10;
+      $users = [];
+
+      for ($i = 0; $i < $limit; $i++) {
+        User::create([
+          'name' => $faker->name,
+          'email' => $faker->unique()->email,
+          'password' => Hash::make('testing')
+        ]);
+      }
+
+      Model::reguard();
         // $this->call(UsersTableSeeder::class);
     }
 }
