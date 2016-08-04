aad1e5064f6cbbcdbdc532d79a68518c0f2f8547
Joseph Krump <krump.joseph@gmail.com>
2016-08-04 00:42:11 -0700

    created migration for widgets

---
+++ database/migrations/2016_08_04_073948_create_widgets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWidgetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('widgets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('row');
+            $table->integer('col');
+            $table->integer('size');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('widgets');
+    }
+}
