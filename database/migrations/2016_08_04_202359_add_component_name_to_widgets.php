d194c4bd1aaa5a19a7536aef98b1f6e5b8d529d9
Joseph Krump <krump.joseph@gmail.com>
2016-08-04 13:24:59 -0700

    Added migration to add component_name field to wigets

---
+++ database/migrations/2016_08_04_202359_add_component_name_to_widgets.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddComponentNameToWidgets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('widgets', function (Blueprint $table) {
+            $table->string('component_name')->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('widgets', function (Blueprint $table) {
+            $table->dropColumn('component_name');
+        });
+    }
+}
