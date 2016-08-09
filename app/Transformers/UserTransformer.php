ee7b1062b2be0a3bf2e917bdd22440dd320e1156
Joseph Krump <krump.joseph@gmail.com>
2016-08-08 20:48:04 -0700

    added UserTransformer with use of dingo api responses

---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,23 @@
+<?php
+
+use League\Fractal\TransformerAbstract;
+use App\User;
+
+class UserTransformer extends TransformerAbstract
+{
+
+    /**
+     * Turn this item object into a generic array
+     *
+     * @return array
+     */
+    public function transform(User $user)
+    {
+        return [
+            'id'            => (int) $user->id,
+            'name'          => $user->name,
+            'email'         => $user->email
+        ];
+    }
+
+}
