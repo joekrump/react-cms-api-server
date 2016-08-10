fe31d794f7d7beebcfa0680b707d71bf1d7a6b5c
Joseph Krump <krump.joseph@gmail.com>
2016-08-10 00:30:11 -0700

    changed indenting to 2 spaces

--- app/Api/V1/Controllers/Admin/PermissionController.php
+++ app/Api/V1/Controllers/Admin/PermissionController.php
@@ -17,116 +17,116 @@ use Validator;
  */
 class PermissionController extends Controller
 {
-    use Helpers;
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Dingo\Api\Http\Response 
-     */
-    public function index()
-    {
-        $permissions = Permission::all();
-        return $this->response->collection($permissions, new PermissionTransformer);
+  use Helpers;
+  /**
+   * Display a listing of the resource.
+   *
+   * @return Dingo\Api\Http\Response 
+   */
+  public function index()
+  {
+    $permissions = Permission::all();
+    return $this->response->collection($permissions, new PermissionTransformer);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $validator = Validator::make($request->only(['name', 'display_name', 'description']), [
+      'name' => 'required|max:100|unique:permissions,name',
+      'display_name' => 'required|max:100|unique:permissions,display_name',
+      'description' => 'required|max:255'
+      ]);
+
+
+    if ($validator->fails()) {
+      throw new \Dingo\Api\Exception\StoreResourceFailedException('Could not create new Permission.', $validator->errors());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $validator = Validator::make($request->only(['name', 'display_name', 'description']), [
-            'name' => 'required|max:100|unique:permissions,name',
-            'display_name' => 'required|max:100|unique:permissions,display_name',
-            'description' => 'required|max:255'
-        ]);
 
+    $permission = new Permission($request->only(['name', 'display_name', 'description']));
 
-        if ($validator->fails()) {
-          throw new \Dingo\Api\Exception\StoreResourceFailedException('Could not create new Permission.', $validator->errors());
-        }
+    if($permission->save())
+      return $this->response->item($permission, new PermissionTransformer)->setStatusCode(200);
+    else
+      return $this->response->errorBadRequest('Could Not Create Permission');
+  }
 
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    if($permission = Permission::find($id)){
+      return $this->response->item($permission, new PermissionTransformer)->setStatusCode(200);
+    } 
+    return  $this->response->errorNotFound('Could Not Find details for Permission with id=' . $id);
+  }
 
-        $permission = new Permission($request->only(['name', 'display_name', 'description']));
-
-        if($permission->save())
-          return $this->response->item($permission, new PermissionTransformer)->setStatusCode(200);
-        else
-          return $this->response->errorBadRequest('Could Not Create Permission');
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $permission = Permission::find($id);
+    if(!$permission) {
+      return $this->response->errorNotFound('Could Not Find Permission with id=' . $id);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        if($permission = Permission::find($id)){
-          return $this->response->item($permission, new PermissionTransformer)->setStatusCode(200);
-        } 
-        return  $this->response->errorNotFound('Could Not Find details for Permission with id=' . $id);
+    $acceptedInput = $request->only(['name', 'display_name', 'description']);
+    
+    $validator = Validator::make($acceptedInput, [
+      'name' => 'max:100|unique:permissions,name,' . $id,
+      'display_name' => 'max:100', 
+      'description' => 'max:255'
+      ]);
+
+    if ($validator->fails()) {
+      throw new \Dingo\Api\Exception\UpdateResourceFailedException('Could not update the permission.', $validator->errors());
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $permission = Permission::find($id);
-        if(!$permission) {
-          return $this->response->errorNotFound('Could Not Find Permission with id=' . $id);
-        }
-
-        $acceptedInput = $request->only(['name', 'display_name', 'description']);
-        
-        $validator = Validator::make($acceptedInput, [
-            'name' => 'max:100|unique:permissions,name,' . $id,
-            'display_name' => 'max:100', 
-            'description' => 'max:255'
-        ]);
-
-        if ($validator->fails()) {
-          throw new \Dingo\Api\Exception\UpdateResourceFailedException('Could not update the permission.', $validator->errors());
-        }
-
-        if($request->has('name')){
-          $permission->name = $request->get('name');
-        }
-        if($request->has('display_name')){
-          $permission->display_name = $request->get('display_name');
-        }
-        if($request->has('description')){
-          $permission->description = $request->get('description');
-        }
-
-        if($permission->save())
-          return $this->response->item($permission, new PermissionTransformer)->setStatusCode(200);
-        else
-          return $this->response->errorBadRequest('Could not Update Permission with id=' . $id);
+    if($request->has('name')){
+      $permission->name = $request->get('name');
+    }
+    if($request->has('display_name')){
+      $permission->display_name = $request->get('display_name');
+    }
+    if($request->has('description')){
+      $permission->description = $request->get('description');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        if($permission = Permission::find($id)) {
-          if($permission->delete())
-            return $this->response->noContent()->setStatusCode(200);
-          else
-            return $this->response->errorBadRequest('Could Note Remove the Permission with id=' . $id);
-        }
-        return $this->response->errorNotFound('Could not Find Permission to remove with an id=' . $id);
+    if($permission->save())
+      return $this->response->item($permission, new PermissionTransformer)->setStatusCode(200);
+    else
+      return $this->response->errorBadRequest('Could not Update Permission with id=' . $id);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    if($permission = Permission::find($id)) {
+      if($permission->delete())
+        return $this->response->noContent()->setStatusCode(200);
+      else
+        return $this->response->errorBadRequest('Could Note Remove the Permission with id=' . $id);
     }
+    return $this->response->errorNotFound('Could not Find Permission to remove with an id=' . $id);
+  }
 }
--- app/Api/V1/Controllers/Admin/RoleController.php
+++ app/Api/V1/Controllers/Admin/RoleController.php
@@ -17,117 +17,117 @@ use Validator;
  */
 class RoleController extends Controller
 {
-    use Helpers;
-    /**
-     * Display a listing of the resource.
-     *
-     * @return Dingo\Api\Http\Response 
-     */
-    public function index()
-    {
-        $roles = Role::all();
-        return $this->response->collection($roles, new RoleTransformer);
+  use Helpers;
+  /**
+   * Display a listing of the resource.
+   *
+   * @return Dingo\Api\Http\Response 
+   */
+  public function index()
+  {
+    $roles = Role::all();
+    return $this->response->collection($roles, new RoleTransformer);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $validator = Validator::make($request->only(['name', 'display_name', 'description']), [
+      'name' => 'required|max:100|unique:roles,name',
+      'display_name' => 'required|max:100|unique:roles,display_name',
+      'description' => 'required|max:255'
+      ]);
+
+
+    if ($validator->fails()) {
+      throw new \Dingo\Api\Exception\StoreResourceFailedException('Could not create new Role.', $validator->errors());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $validator = Validator::make($request->only(['name', 'display_name', 'description']), [
-            'name' => 'required|max:100|unique:roles,name',
-            'display_name' => 'required|max:100|unique:roles,display_name',
-            'description' => 'required|max:255'
-        ]);
 
+    $role = new Role($request->only(['name', 'display_name', 'description']));
 
-        if ($validator->fails()) {
-          throw new \Dingo\Api\Exception\StoreResourceFailedException('Could not create new Role.', $validator->errors());
-        }
+    if($role->save())
+      return $this->response->item($role, new RoleTransformer)->setStatusCode(200);
+    else
+      return $this->response->errorBadRequest('Could Not Create Role');
+  }
 
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    if($role = Role::find($id)){
+      return $this->response->item($role, new RoleTransformer)->setStatusCode(200);
+    } 
+    return  $this->response->errorNotFound('Could Not Find details for Role with id=' . $id);
+  }
 
-        $role = new Role($request->only(['name', 'display_name', 'description']));
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
 
-        if($role->save())
-          return $this->response->item($role, new RoleTransformer)->setStatusCode(200);
-        else
-          return $this->response->errorBadRequest('Could Not Create Role');
+    $role = Role::find($id);
+    if(!$role) {
+      return $this->response->errorNotFound('Could Not Find Role with id=' . $id);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        if($role = Role::find($id)){
-          return $this->response->item($role, new RoleTransformer)->setStatusCode(200);
-        } 
-        return  $this->response->errorNotFound('Could Not Find details for Role with id=' . $id);
+    $acceptedInput = $request->only(['name', 'display_name', 'description']);
+    
+    $validator = Validator::make($acceptedInput, [
+      'name' => 'max:100|unique:roles,name,' . $id,
+      'display_name' => 'max:100|unique:roles,display_name,' . $id,
+      'description' => 'max:255'
+      ]);
+
+    if ($validator->fails()) {
+      throw new \Dingo\Api\Exception\UpdateResourceFailedException('Could not update the role.', $validator->errors());
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-
-        $role = Role::find($id);
-        if(!$role) {
-          return $this->response->errorNotFound('Could Not Find Role with id=' . $id);
-        }
-
-        $acceptedInput = $request->only(['name', 'display_name', 'description']);
-        
-        $validator = Validator::make($acceptedInput, [
-            'name' => 'max:100|unique:roles,name,' . $id,
-            'display_name' => 'max:100|unique:roles,display_name,' . $id,
-            'description' => 'max:255'
-        ]);
-
-        if ($validator->fails()) {
-          throw new \Dingo\Api\Exception\UpdateResourceFailedException('Could not update the role.', $validator->errors());
-        }
-
-        if($request->has('name')){
-          $role->name = $request->get('name');
-        }
-        if($request->has('display_name')){
-          $role->display_name = $request->get('display_name');
-        }
-        if($request->has('description')){
-          $role->description = $request->get('description');
-        }
-
-        if($role->save())
-          return $this->response->item($role, new RoleTransformer)->setStatusCode(200);
-        else
-          return $this->response->errorBadRequest('Could not Update Role with id=' . $id);
+    if($request->has('name')){
+      $role->name = $request->get('name');
+    }
+    if($request->has('display_name')){
+      $role->display_name = $request->get('display_name');
+    }
+    if($request->has('description')){
+      $role->description = $request->get('description');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        if($role = Role::find($id)) {
-          if($role->delete())
-            return $this->response->noContent()->setStatusCode(200);
-          else
-            return $this->response->errorBadRequest('Could Note Remove the Role with id=' . $id);
-        }
-        return $this->response->errorNotFound('Could not Find Role to remove with an id=' . $id);
+    if($role->save())
+      return $this->response->item($role, new RoleTransformer)->setStatusCode(200);
+    else
+      return $this->response->errorBadRequest('Could not Update Role with id=' . $id);
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    if($role = Role::find($id)) {
+      if($role->delete())
+        return $this->response->noContent()->setStatusCode(200);
+      else
+        return $this->response->errorBadRequest('Could Note Remove the Role with id=' . $id);
     }
+    return $this->response->errorNotFound('Could not Find Role to remove with an id=' . $id);
+  }
 }
