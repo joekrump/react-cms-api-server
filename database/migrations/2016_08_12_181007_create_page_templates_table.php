9b3a584ee3c86dbc88add5f69d3a06508cbe72bb
Joseph Krump <krump.joseph@gmail.com>
2016-08-12 11:11:12 -0700

    Generated PageTemplate model and migration

---
+++ database/migrations/2016_08_12_181007_create_page_templates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePageTemplatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page_templates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('display_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('page_templates');
+    }
+}
