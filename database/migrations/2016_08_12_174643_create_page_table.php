431c6795af96b72b0c56101e2d6486c335d907fa
Joseph Krump <krump.joseph@gmail.com>
2016-08-12 11:04:31 -0700

    Added Page model and migration

---
+++ database/migrations/2016_08_12_174643_create_page_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_id')->unsigned()->nullable();
+            $table->foreign('parent_id')->references('id')->on('pages');
+            $table->string('full_path')->unique();
+            $table->boolean('in_menu')->default(false);
+            $table->boolean('deleteable')->default(true);
+            $table->boolean('draft')->default(true);
+            $table->integer('position');
+            $table->integer('template_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pages');
+    }
+}
