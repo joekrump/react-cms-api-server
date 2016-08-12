311c3d9e9216eeb6559a2e781e0606ec9f4cb81e
Joseph Krump <krump.joseph@gmail.com>
2016-08-12 11:07:58 -0700

    Added PagePart model and migration

---
+++ database/migrations/2016_08_12_180701_create_page_parts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePagePartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page_parts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('page_id')->unsigned();
+            $table->foreign('page_id')->references('id')->on('pages');
+            $table->string('title');
+            $table->text('content');
+            $table->integer('position');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('page_parts');
+    }
+}
