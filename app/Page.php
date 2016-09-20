8a03908b23beff7cb038abb675fcff0d58140d93
Joseph Krump <krump.joseph@gmail.com>
2016-09-20 15:31:28 -0700

    using Sluggable in Page model

--- app/Page.php
+++ app/Page.php
@@ -3,46 +3,49 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Page extends Model
 {
-    protected $fillable = [
-      'full_path',
-      'in_menu',
-      'deletable',
-      'depth',
-      'draft',
-      'slug',
-      'position',
-      'template_id'
-    ];
+  use Sluggable;
 
-    public function children() {
-      return $this->hasMany('App\Page', 'parent_id', 'id');
+  protected $fillable = [
+    'full_path',
+    'in_menu',
+    'deletable',
+    'depth',
+    'draft',
+    'slug',
+    'position',
+    'template_id'
+  ];
+
+  public function children() {
+    return $this->hasMany('App\Page', 'parent_id', 'id');
+  }
+
+  public function parent()
+  {
+      return $this->belongsTo('App\Page', 'parent_id');
+  }
+
+  public function template()
+  {
+      return $this->belongsTo('App\PageTemplate', 'template_id');
+  }
+
+  public function parts()
+  {
+      return $this->hasMany('App\PagePart');
+  }
+
+  public function contents() {
+    $partsWithContent = '';
+
+    foreach($this->parts as $part) {
+      $partsWithContent .= $part->content;
     }
 
-    public function parent()
-    {
-        return $this->belongsTo('App\Page', 'parent_id');
-    }
-
-    public function template()
-    {
-        return $this->belongsTo('App\PageTemplate', 'template_id');
-    }
-
-    public function parts()
-    {
-        return $this->hasMany('App\PagePart');
-    }
-
-    public function contents() {
-      $partsWithContent = '';
-
-      foreach($this->parts as $part) {
-        $partsWithContent .= $part->content;
-      }
-
-      return $partsWithContent;
-    }
+    return $partsWithContent;
+  }
 }
