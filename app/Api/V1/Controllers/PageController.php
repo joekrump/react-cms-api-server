07db17e07f8a7e7f72d7f0214afc7800bce2e9d3
Joseph Krump <krump.joseph@gmail.com>
2016-09-20 16:07:37 -0700

    changed indenting to 2 spaces in PageController

--- app/Api/V1/Controllers/PageController.php
+++ app/Api/V1/Controllers/PageController.php
@@ -12,106 +12,106 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class PageController extends Controller
 {
-    use Helpers;
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+  use Helpers;
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+      //
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+      //
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+      //
+  }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+      //
+  }
 
-    /**
-     * Find a page based on the fullpath provided in a query param.
-     * @param  Request $request [description]
-     * @return [type]           [description]
-     */
-    public function by_path(Request $request)
-    {
-        $pagePath = $request->get('fullpath');
+  /**
+   * Find a page based on the fullpath provided in a query param.
+   * @param  Request $request [description]
+   * @return [type]           [description]
+   */
+  public function by_path(Request $request)
+  {
+    $pagePath = $request->get('fullpath');
 
-        if(!$pagePath) {
-           return $this->response->errorNotFound('Could not Find Page'); 
-        } else {
-            if($pagePath[0] != '/') {
-                $pagePath = '/' . $pagePath;
-            }
-            try {
-                $page = Page::where('full_path', $pagePath)->firstOrFail();
-                return $this->response->item($page, new PageTransformer)->setStatusCode(200);
-            } catch (ModelNotFoundException $e) {
-                return $this->response->errorNotFound('Could not Find Page: ' . $pagePath);
-            } catch (Exception $e){
-                return $this->response->errorBadRequest($e);
-            }
-        }
+    if(!$pagePath) {
+     return $this->response->errorNotFound('Could not Find Page'); 
+   } else {
+    if($pagePath[0] != '/') {
+      $pagePath = '/' . $pagePath;
     }
+    try {
+      $page = Page::where('full_path', $pagePath)->firstOrFail();
+      return $this->response->item($page, new PageTransformer)->setStatusCode(200);
+    } catch (ModelNotFoundException $e) {
+      return $this->response->errorNotFound('Could not Find Page: ' . $pagePath);
+    } catch (Exception $e){
+      return $this->response->errorBadRequest($e);
+    }
+  }
+}
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+      //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+      //
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+      //
+  }
 }
