92351597d58b3ed20c5b47cf7d3c251c802fb9cd
Joseph Krump <krump.joseph@gmail.com>
2016-09-27 21:23:12 -0700

    migration to add position to books added

---
+++ database/migrations/2016_09_28_042257_add_position_to_books.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPositionToBooks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            //
+        });
+    }
+}
