cf6b514abfb0a5782ca1492a8a0f87e3e1c6381c
Joseph Krump <krump.joseph@gmail.com>
2016-09-27 21:28:01 -0700

    added migration to add position to roles

---
+++ database/migrations/2016_09_28_042357_add_position_to_roles.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPositionToRoles extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            //
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('roles', function (Blueprint $table) {
+            //
+        });
+    }
+}
