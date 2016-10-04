9b8ec08df29528958eb7511db587a18ab3c7682d
Joseph Krump <krump.joseph@gmail.com>
2016-10-04 13:01:38 -0700

    Added CardTransformer

---
+++ app/Transformers/CardTransformer.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+use App\Card;
+
+class CardTransformer extends TransformerAbstract
+{
+  /**
+   * Turn this item object into a generic array
+   *
+   * @return array
+   */
+  public function transform(Card $card)
+  {
+    $substrLength = 20;
+    return [
+    'id'            => $card->id,
+    'front_content' => $card->front_content,
+    'back_content'  => $card->back_content,
+    'primary'       => substr($card->front_content, 0, $substrLength),
+    'secondary'     => substr($card->back_content, 0, $substrLength)
+    ];
+  }
+
+}
