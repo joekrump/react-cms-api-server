0e6487565feb116116d7ddfccf00c70b8ca5b74a
Joseph Krump <krump.joseph@gmail.com>
2016-10-20 14:44:11 -0700

    added unauthenticated exception Handler method

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -11,48 +11,64 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that should not be reported.
-     *
-     * @var array
-     */
-    protected $dontReport = [
-      AuthorizationException::class,
-      HttpException::class,
-      ModelNotFoundException::class,
-      ValidationException::class,
-    ];
+  /**
+   * A list of the exception types that should not be reported.
+   *
+   * @var array
+   */
+  protected $dontReport = [
+    AuthorizationException::class,
+    HttpException::class,
+    ModelNotFoundException::class,
+    ValidationException::class,
+  ];
 
-    /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $e
-     * @return void
-     */
-    public function report(Exception $e)
-    {
-      parent::report($e);
-    }
-
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $e)
-    {
-      if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException)
-      {
-        return response(['Token is invalid'], $e->getStatusCode());
-      }
-      if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException)
-      {
-        return response(['Token has expired'], $e->getStatusCode());
-      }
-      return parent::render($request, $e);
-    }
+  /**
+   * Report or log an exception.
+   *
+   * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+   *
+   * @param  \Exception  $e
+   * @return void
+   */
+  public function report(Exception $e)
+  {
+    parent::report($e);
   }
+
+  /**
+   * Render an exception into an HTTP response.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Exception  $e
+   * @return \Illuminate\Http\Response
+   */
+  public function render($request, Exception $e)
+  {
+    if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenInvalidException)
+    {
+      return response(['Token is invalid'], $e->getStatusCode());
+    }
+    if ($e instanceof \Tymon\JWTAuth\Exceptions\TokenExpiredException)
+    {
+      return response(['Token has expired'], $e->getStatusCode());
+    }
+    return parent::render($request, $e);
+  }
+
+  /**
+   * Convert an authentication exception into an unauthenticated response.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Illuminate\Auth\AuthenticationException  $exception
+   * @return \Illuminate\Http\Response
+   */
+  protected function unauthenticated($request, AuthenticationException $exception)
+  {
+      if ($request->expectsJson()) {
+          return response()->json(['error' => 'Unauthenticated.'], 401);
+      }
+
+      return redirect()->guest('login');
+  }
+}
