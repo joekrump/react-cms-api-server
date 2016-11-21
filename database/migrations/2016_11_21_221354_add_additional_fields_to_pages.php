ce2bf4f3c5729f57f6a91e8370fc17023ca9085d
Joseph Krump <krump.joseph@gmail.com>
2016-11-21 14:19:48 -0800

    added additional fields for pages table

---
+++ database/migrations/2016_11_21_221354_add_additional_fields_to_pages.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAdditionalFieldsToPages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->string('image_url')->nullable();
+            $table->text('summary');
+            $table->boolean('show_title')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('pages', function (Blueprint $table) {
+            $table->string('image_url')->nullable();
+            $table->text('summary');
+            $table->boolean('show_title')->default(true);
+        });
+    }
+}
