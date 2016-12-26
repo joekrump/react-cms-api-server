b2d97b5fca327f13ed2e934c89b92432048c911f
Joseph Krump <krump.joseph@gmail.com>
2016-12-25 17:27:13 -0800

    PageTransformer updated to return child pages

--- app/Transformers/PageTransformer.php
+++ app/Transformers/PageTransformer.php
@@ -8,33 +8,58 @@ use App\PageTemplate;
 
 class PageTransformer extends TransformerAbstract
 {
-  /**
-   * Turn this item object into a generic array
-   *
-   * @return array
-   */
-  public function transform(Page $page)
-  {
 
-      return [
-        'id'              => $page->id,
-        'name'            => $page->name,
-        'full_path'       => $page->full_path,
-        'slug'            => $page->slug,
-        'in_menu'         => $page->in_menu,
-        'depth'           => $page->depth,
-        'position'        => $page->position,
-        'deletable'       => $page->deletable,
-        'draft'           => $page->draft,
-        'template_id'     => $page->template_id,
-        'primary'         => $page->name,
-        'use_editor'      => true,
-        'content'         => $page->contents(),
-        'summary'         => $page->summary,
-        'show_title'      => $page->show_title,
-        'image_url'       => $page->image_url,
-        'templates'       => PageTemplate::orderBy('display_name', 'asc')->get(['display_name', 'id'])
-        // 'secondary'   => $page->full_path
-      ];
+  public function getDefaultFields(Page $page) {
+    return [
+      'id'              => $page->id,
+      'name'            => $page->name,
+      'full_path'       => $page->full_path,
+      'slug'            => $page->slug,
+      'in_menu'         => $page->in_menu,
+      'depth'           => $page->depth,
+      'position'        => $page->position,
+      'deletable'       => $page->deletable,
+      'draft'           => $page->draft,
+      'template_id'     => $page->template_id,
+      'primary'         => $page->name,
+      'use_editor'      => true,
+      'content'         => $page->contents(),
+      'summary'         => $page->summary,
+      'show_title'      => $page->show_title,
+      'image_url'       => $page->image_url,
+      'templates'       => PageTemplate::orderBy('display_name', 'asc')->get(['display_name', 'id']),
+      'children'        => [],
+    ];
+  }
+
+  public function getBasicFields(Page $page) {
+    return [
+      'id'              => $page->id,
+      'name'            => $page->name,
+      'full_path'       => $page->full_path,
+      'summary'         => $page->summary,
+      'image_url'       => $page->image_url,
+    ];
+  }
+
+  public function transform(Page $page, $isChildPage = false)
+  {
+    if($page->children->count() > 0 && !$isChildPage) {
+      $page_with_children = $this->getDefaultFields($page);
+      
+      $childPages = $page->children()
+        ->orderBy('depth', 'asc')
+        ->orderBy('position', 'asc')
+        ->orderBy('name', 'asc')->get();
+      
+      foreach ($childPages as $key => $value) {
+         $page_with_children['children'][] = $this->transform($value, true);
+      }
+      return $page_with_children;
+    } else if($isChildPage) {
+      return $this->getBasicFields($page);
+    } else {
+      return $this->getDefaultFields($page);
+    }
   }
 }
